a59f9a6e2276ce433680d02a70eb5a02c1af56bd
RAC IT SOLUTIONS PVT.LTD <racitsolutionspvtltd@racs-mac-mini.local>
2017-07-13 14:46:48 +0530

    Initial Commit

---
+++ SecondGitDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SecondGitDemo
+//
+//  Created by RAC IT SOLUTIONS PVT.LTD on 13/07/17.
+//  Copyright © 2017 NetConnect. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
